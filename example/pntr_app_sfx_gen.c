c1f73fa89137eefcd4c250b4a0df21b27be30e83
David Konsumer <konsumer@jetboystudio.com>
2024-07-06 20:56:02 -0700

    log code on save

--- example/pntr_app_sfx_gen.c
+++ example/pntr_app_sfx_gen.c
@@ -27,10 +27,74 @@ EM_ASYNC_JS(void, download_rfx_file, (unsigned char* dataPtr, int size, char* su
     await writableStream.write(blob);
     await writableStream.close();
   } catch (e) {
-    console.log('aborted.');
+    console.log('rfx file-save aborted.');
   }
 });
 #endif  // EMSCRIPTEN
+
+// output the code on the console
+void print_code(SfxParams sfx_params) {
+  printf(
+      "#define PNTR_APP_SFX_IMPLEMENTATION\n\
+#include \"pntr_app_sfx.h\"\n\
+\n\
+SfxParams gen_sound_effect() {\n\
+  return (SfxParams) {\n\
+    .randSeed=%ff,\n\
+    .waveType=%d,\n\
+    .attackTime=%ff,\n\
+    .sustainTime=%ff,\n\
+    .sustainPunch=%ff,\n\
+    .decayTime=%ff,\n\
+    .startFrequency=%ff,\n\
+    .minFrequency=%ff,\n\
+    .slide=%ff,\n\
+    .deltaSlide=%ff,\n\
+    .vibratoDepth=%ff,\n\
+    .vibratoSpeed=%ff,\n\
+    .changeAmount=%ff,\n\
+    .changeSpeed=%ff,\n\
+    .squareDuty=%ff,\n\
+    .dutySweep=%ff,\n\
+    .repeatSpeed=%ff,\n\
+    .phaserOffset=%ff,\n\
+    .phaserSweep=%ff,\n\
+    .lpfCutoff=%ff,\n\
+    .lpfCutoffSweep=%ff,\n\
+    .lpfResonance=%ff,\n\
+    .hpfCutoff=%ff,\n\
+    .hpfCutoffSweep=%ff };\n\
+}\n",
+      sfx_params.randSeed, sfx_params.waveType, sfx_params.attackTime, sfx_params.sustainTime, sfx_params.sustainPunch, sfx_params.decayTime, sfx_params.startFrequency, sfx_params.minFrequency, sfx_params.slide, sfx_params.deltaSlide, sfx_params.vibratoDepth, sfx_params.vibratoSpeed, sfx_params.changeAmount, sfx_params.changeSpeed, sfx_params.squareDuty, sfx_params.dutySweep, sfx_params.repeatSpeed, sfx_params.phaserOffset, sfx_params.phaserSweep, sfx_params.lpfCutoff, sfx_params.lpfCutoffSweep, sfx_params.lpfResonance, sfx_params.hpfCutoff, sfx_params.hpfCutoffSweep);
+}
+
+SfxParams gen_sound_effect() {
+  return (SfxParams){
+      .randSeed = 0.000000f,
+      .waveType = 0,
+      .attackTime = 0.204667f,
+      .sustainTime = 0.000000f,
+      .sustainPunch = 0.255497f,
+      .decayTime = 0.487662f,
+      .startFrequency = 0.000000f,
+      .minFrequency = 0.209907f,
+      .slide = 0.000000f,
+      .deltaSlide = 0.000000f,
+      .vibratoDepth = 0.000000f,
+      .vibratoSpeed = 0.000000f,
+      .changeAmount = 0.000000f,
+      .changeSpeed = 0.437060f,
+      .squareDuty = 0.000000f,
+      .dutySweep = 0.000000f,
+      .repeatSpeed = 0.000000f,
+      .phaserOffset = 0.000000f,
+      .phaserSweep = 1.000000f,
+      .lpfCutoff = 0.000000f,
+      .lpfCutoffSweep = 0.000000f,
+      .lpfResonance = 0.194383f,
+      .hpfCutoff = 0.000000f,
+      .hpfCutoffSweep = 0.000000f};
+}
 
 bool Init(pntr_app* app) {
   AppData* appData = pntr_load_memory(sizeof(AppData));
@@ -107,8 +171,6 @@ bool Update(pntr_app* app, pntr_image* screen) {
   }
   nk_end(ctx);
 
-#ifdef EMSCRIPTEN
-
   // Play
   if (nk_begin(ctx, "Play", nk_rect(screen->width / 3, 0, (screen->width / 3), screen->height / 6), NK_WINDOW_NO_SCROLLBAR)) {
     nk_layout_row_dynamic(ctx, 0, 1);
@@ -122,6 +184,9 @@ bool Update(pntr_app* app, pntr_image* screen) {
   if (nk_begin(ctx, "Save", nk_rect((screen->width / 3) + (screen->width / 3), 0, (screen->width / 3), screen->height / 6), NK_WINDOW_NO_SCROLLBAR)) {
     nk_layout_row_dynamic(ctx, 0, 1);
     if (nk_button_label(ctx, "Save")) {
+      print_code(appData->sfx_params);
+
+#ifdef EMSCRIPTEN
       // build file-string
       unsigned char fileData[104] = {0};
       char* signature = "rFX ";
@@ -133,22 +198,10 @@ bool Update(pntr_app* app, pntr_image* screen) {
       PNTR_MEMCPY(fileData + 8, &appData->sfx_params, 96);
 
       download_rfx_file(fileData, 104, "sound.rfx", "downloads", "octet/stream");
+#endif  // EMSCRIPTEN
     }
   }
   nk_end(ctx);
-
-#else
-
-  // Play
-  if (nk_begin(ctx, "Play", nk_rect(screen->width / 3, 0, (screen->width / 3) * 2, screen->height / 6), NK_WINDOW_NO_SCROLLBAR)) {
-    nk_layout_row_dynamic(ctx, 0, 1);
-    if (nk_button_label(ctx, "Play")) {
-      pntr_app_sfx_gen_play(app);
-    }
-  }
-  nk_end(ctx);
-
-#endif
 
   // Configure
   if (nk_begin(ctx, "Configure", nk_rect(screen->width / 3, screen->height / 6, (screen->width / 3) * 2, screen->height - (screen->height / 6)), 0)) {
